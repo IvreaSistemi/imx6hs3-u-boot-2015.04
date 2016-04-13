f896820f29bf1b7b29e927ddd62eb51ca0212f9f
De Lucia Fabio <delucia@ivreasistemi.it>
2016-04-13 11:50:54 +0200

    Initial version

---
+++ mx6solosabresd_config/include/config.h
@@ -0,0 +1,11 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/mx6sabresd/mx6solo_4x_mt41j128.cfg
+#define CONFIG_MX6SOLO	1
+#define CONFIG_SYS_USE_SPINOR	1
+#define CONFIG_SYS_NOSMP	"nosmp"
+#define CONFIG_BOARDDIR board/freescale/mx6sabresd
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6sabresd.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ mx6solosabresd_config/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Apr 11 2016"
+#define U_BOOT_TIME "12:51:36"
